967d2a4a760eadd4662b035ef386a2b7d6da6df1
Diana Martseniuk <diana.martsenyuk@gmail.com>
2026-03-31 17:41:34 +0200

    Add displaying task list

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ApbdCw1Git;
+
+List<TodoTask> tasks = new()
+{
+    new TodoTask("Buy groceries"),
+    new TodoTask("Finish homework")
+};
+
+ShowTasks(tasks);
+
+static void ShowTasks(List<TodoTask> tasks)
+{
+    if (tasks.Count == 0)
+    {
+        Console.WriteLine("Task list is empty.");
+        return;
+    }
+
+    Console.WriteLine("Tasks:");
+
+    for (int i = 0; i < tasks.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {tasks[i].Title}");
+    }
+}
