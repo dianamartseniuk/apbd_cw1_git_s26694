ef52172698f9df8b27ba0a6444557654bb32b605
Diana Martseniuk <diana.martsenyuk@gmail.com>
2026-03-31 21:20:27 +0200

    Add IsCompleted property to TodoTask

--- TodoTask.cs
+++ TodoTask.cs
@@ -3,9 +3,11 @@ namespace ApbdCw1Git;
 public class TodoTask
 {
     public string Title { get; set; }
+    public bool IsCompleted { get; set; }
 
     public TodoTask(string title)
     {
         Title = title;
+        IsCompleted = false;
     }
 }
